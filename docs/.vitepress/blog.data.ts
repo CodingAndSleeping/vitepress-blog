81f10269a2dc0e5466cde9ed90075f9ac298cc13
lzt <1162300237@qq.com>
2025-04-23 21:45:03 +0800

    feat: label card

--- docs/.vitepress/blog.data.ts
+++ docs/.vitepress/blog.data.ts
@@ -42,25 +42,10 @@ export default createContentLoader('/**/*.md', {
 });
 
 function formatDate(raw: string): Data['date'] {
-  // const date = new Date(raw);
-  // date.setUTCHours(12);
-
   const date = moment.utc(raw).hours(12);
   return {
-    // time: +date,
-    // ZHString: date.toLocaleDateString('zh-CN', {
-    //   year: 'numeric',
-    //   month: 'long',
-    //   day: 'numeric',
-    // }),
-    // ENString: date.toLocaleDateString('en-US', {
-    //   year: 'numeric',
-    //   month: 'long',
-    //   day: 'numeric',
-    // }),
-
     time: date.valueOf(), // 获取时间戳（毫秒）
-    ZHString: date.locale('zh-CN').format('YYYY年M月D日'), // 中文格式：2025年4月21日
-    ENString: date.locale('en').format('MMMM D, YYYY'), // 英文格式：April 21, 2025
+    ZHString: date.locale('zh-CN').format('YYYY年M月D日'), // 中文格式：xxxx年xx月xx日
+    ENString: date.locale('en').format('MMMM D, YYYY'), // 英文格式：April xx, xxxx
   };
 }
