1163a9f10b48544de7223cd5c089c5a6a10417cb
lzt <1162300237@qq.com>
2025-04-24 01:06:16 +0800

    feat: add auto sidebar plugin

--- docs/.vitepress/config.mts
+++ docs/.vitepress/config.mts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vitepress';
 import { fileURLToPath, URL } from 'node:url';
 import themeConfig from './themeConfig';
+
+import AutoSidebar from 'vite-plugin-vitepress-auto-sidebar';
+import { de } from 'element-plus/es/locales.mjs';
 // https://vitepress.dev/reference/site-config
+
 export default defineConfig({
   base: '/vitepress-blog/',
   srcDir: 'data',
   title: 'CodingAndSleeping',
-  // description: "A VitePress Site",
   lastUpdated: true,
   head: [
     ['link', { rel: 'icon', href: '/vitepress-blog/favicon.svg' }],
@@ -30,5 +33,38 @@ export default defineConfig({
         },
       ],
     },
+
+    plugins: [
+      AutoSidebar({
+        ignoreList: ['**/public/**'],
+        path: 'docs/data',
+        ignoreIndexItem: true,
+        titleFromFileByYaml: true,
+        beforeCreateSideBarItems(data) {
+          data.sort((a, b) => {
+            return Number(a.split('.')[0]) - Number(b.split('.')[0]);
+          });
+          console.log(data);
+
+          return data;
+        },
+
+        sideBarResolved(data) {
+          const newData = {};
+          Object.entries(data).forEach(([key, value]) => {
+            const items = value[0].items;
+            items.forEach((item) => {
+              if (item.items) {
+                newData[`${key}${item.text}/`] = item.items;
+              }
+              if (item.link) {
+                newData[key] = items;
+              }
+            });
+          });
+          return newData;
+        },
+      }),
+    ],
   },
 });
--- docs/.vitepress/themeConfig.ts
+++ docs/.vitepress/themeConfig.ts
@@ -4,54 +4,82 @@ const nav: DefaultTheme.Config['nav'] = [
   {
     text: '前端知识',
     items: [
-      { text: 'JavaScript', link: '/前端/JavaScript/' },
-      { text: 'HTML', link: '/前端/HTML/' },
-      { text: 'CSS', link: '/前端/CSS/' },
-      { text: 'TypeScript', link: '/前端/TypeScript/' },
-      { text: 'Vue', link: '/前端/Vue/' },
-      { text: 'React', link: '/前端/React/' },
+      {
+        text: 'JavaScript',
+        link: '/前端知识/JavaScript/',
+      },
+      {
+        text: 'HTML',
+        link: '/前端知识/HTML/',
+      },
+      {
+        text: 'CSS',
+        link: '/前端知识/CSS/',
+      },
+      {
+        text: 'TypeScript',
+        link: '/前端知识/TypeScript/',
+      },
+      {
+        text: 'Vue',
+        link: '/前端知识/Vue/',
+      },
+      {
+        text: 'React',
+        link: '/前端知识/React/',
+      },
     ],
   },
-
   {
     text: '后端知识',
     items: [
-      { text: 'Node', link: '/后端/Node/node_study1' },
-      { text: 'Nest', link: '/后端/Nest/' },
-      { text: 'Express', link: '/后端/Express/' },
-      { text: 'Mysql', link: '/后端/MySQL/' },
+      {
+        text: 'Node',
+        link: '/后端知识/Node/1.Node.js(介绍)',
+      },
+      {
+        text: 'Express',
+        link: '/后端知识/Express/',
+      },
+      {
+        text: 'MySQL',
+        link: '/后端知识/MySQL/',
+      },
+      {
+        text: 'Nest',
+        link: '/后端知识/Nest/',
+      },
     ],
   },
   {
     text: '前端工程化',
-    link: '/前端工程化/模块化.md',
+    link: '/前端工程化/1.前端模块化',
   },
   {
     text: '手撕代码',
-    link: '/手撕代码/new',
+    link: '/手撕代码/1.手写 new',
   },
   {
     text: '源码学习',
     items: [
       {
         text: 'Vue',
-        link: '/SourceCode/Vue/',
+        link: '/源码学习/Vue/',
       },
     ],
   },
-
   {
     text: '浏览器',
-    link: '/浏览器/事件循环/',
+    link: '/浏览器/1.事件循环机制',
   },
   {
     text: '网络通信',
-    link: '/网络通信/OSI七层网络模型',
+    link: '/网络通信/1.OSI七层网络模型',
   },
 
   {
     text: '其他',
-    link: '/其他/react+mapboxgl地图文档.md',
+    link: '/其他/1.一个基于 React + MapboxGL 的地图组件库',
   },
 
   {
@@ -79,191 +107,6 @@ const nav: DefaultTheme.Config['nav'] = [
   },
 ];
 
-const sidebar: DefaultTheme.Config['sidebar'] = {
-  '/后端/Node/': [
-    {
-      text: 'Node.js(介绍)',
-      link: '/后端/Node/node_study1',
-    },
-    {
-      text: 'Node.js(npm)',
-      link: '/后端/Node/node_study2',
-    },
-    {
-      text: 'Node.js(全局变量)',
-      link: '/后端/Node/node_study3',
-    },
-    {
-      text: 'Node.js(path)',
-      link: '/后端/Node/node_study4',
-    },
-    {
-      text: 'Node.js(os)',
-      link: '/后端/Node/node_study5',
-    },
-    {
-      text: 'Node.js(process)',
-      link: '/后端/Node/node_study6',
-    },
-    {
-      text: 'Node.js(child_process)',
-      link: '/后端/Node/node_study7',
-    },
-    {
-      text: 'Node.js(events)',
-      link: '/后端/Node/node_study8',
-    },
-    {
-      text: 'Node.js(util)',
-      link: '/后端/Node/node_study9',
-    },
-    {
-      text: 'Node.js(fs)',
-      link: '/后端/Node/node_study10',
-    },
-  ],
-  '/前端工程化/': [
-    {
-      text: '模块化',
-      link: '/前端工程化/模块化',
-    },
-    {
-      text: 'Webpack',
-      link: '/前端工程化/Webpack',
-    },
-  ],
-  '/浏览器/': [
-    {
-      text: '事件循环',
-      link: '/浏览器/事件循环',
-    },
-
-    {
-      text: '跨标签页通信',
-      link: '/浏览器/跨标签页通信',
-    },
-  ],
-  '/网络通信/': [
-    {
-      text: 'OSI七层网络模型',
-      link: '/网络通信/OSI七层网络模型',
-    },
-    {
-      text: 'TCP三次握手与四次挥手',
-      link: '/网络通信/TCP三次握手与四次挥手',
-    },
-    {
-      text: 'DNS 解析规则',
-      link: '/网络通信/DNS解析规则',
-    },
-    {
-      text: 'OPTIONS 预检请求',
-      link: '/网络通信/OPTIONS预检请求',
-    },
-    {
-      text: '浏览器缓存',
-      link: '/网络通信/浏览器缓存机制',
-    },
-
-    {
-      text: 'HTTP1.0、1.1、2.0的区别',
-      link: '/网络通信/HTTP1.0、1.1、2.0的区别',
-    },
-
-    // {
-    //   text: 'HTTP、HTTPS的区别',
-    //   link: '/网络通信/HTTP、HTTPS的区别',
-    // },
-  ],
-
-  '/手撕代码/': [
-    {
-      text: '手写new',
-      link: '/手撕代码/new',
-    },
-    {
-      text: '手写instanceof',
-      link: '/手撕代码/instanceof',
-    },
-    {
-      text: '手写bind-apply-call',
-      link: '/手撕代码/bind-apply-call',
-    },
-    {
-      text: '手写deepclone',
-      link: '/手撕代码/deepclone',
-    },
-    {
-      text: '手写debounce',
-      link: '/手撕代码/debounce',
-    },
-    {
-      text: '手写throttle',
-      link: '/手撕代码/throttle',
-    },
-    {
-      text: '手写promise',
-      link: '/手撕代码/promise',
-    },
-    {
-      text: '手写lazyBoy',
-      link: '/手撕代码/lazyBoy',
-    },
-    {
-      text: '手写洗牌算法',
-      link: '/手撕代码/durstenfeldShuffle',
-    },
-    {
-      text: '手写Promise.all/race',
-      link: '/手撕代码/promiseAll',
-    },
-    {
-      text: '求数组除自身以外乘积',
-      link: '/手撕代码/productExceptSelf',
-    },
-    {
-      text: '扁平化数组',
-      link: '/手撕代码/flattenArr',
-    },
-    {
-      text: '红绿灯',
-      link: '/手撕代码/red-green-light',
-    },
-    {
-      text: '大数相加',
-      link: '/手撕代码/bigint',
-    },
-
-    {
-      text: '无缝轮播图',
-      link: '/手撕代码/无缝轮播图',
-    },
-  ],
-
-  '/其他/': [
-    {
-      text: '地图组件库',
-      link: '/其他/react+mapboxgl地图文档',
-    },
-    {
-      text: 'mapserver地图切片',
-      link: '/其他/基于mapserver的切片缓存服务配置（mapchache.xml文件）',
-    },
-    {
-      text: '一个简单的虚拟滚动',
-      link: '/其他/简易版虚拟滚动',
-    },
-    {
-      text: '简述浏览器中的位置',
-      link: '/其他/浏览器中的位置信息',
-    },
-    {
-      text: 'js小工具',
-      link: '/其他/js小工具',
-    },
-  ],
-};
-
 export default {
   // https://vitepress.dev/reference/default-theme-config
   logo: '/favicon.svg',
@@ -279,7 +122,6 @@ export default {
   },
 
   nav,
-  sidebar,
 
   title: 'CAS BLOG',
   blogLabels: [
